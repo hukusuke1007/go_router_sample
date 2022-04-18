9f2b13fb9c41e564b1841f11329724fd315c023f
Shohei Nakagawa <hukusuke1007@gmail.com>
2022-04-18 22:39:42 +0900

    update

--- README.md
+++ README.md
@@ -1,16 +1,10 @@
 # go_router_sample
 
-go_router sample project
+## Reference
 
-## Getting Started
+[go_router](https://pub.dev/packages/go_router)
 
-This project is a starting point for a Flutter application.
+[gorouter.dev](https://gorouter.dev/)
 
-A few resources to get you started if this is your first Flutter project:
+[go_router 公式ドキュメント日本語版の目次](https://zenn.dev/inari_sushio/scraps/01ef7604a4b934)
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
---
+++ lib/pages/main_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ホーム'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
